5a090874ccddd0986ef39300fe25e0672ba2f094
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-01-18 06:31:01 +0000

    Add checkerboard texture on tabletop

--- handy_warehouse_demo/src/generate_texture.py
+++ handy_warehouse_demo/src/generate_texture.py
@@ -1,28 +1,50 @@
-"""Script that generates a 512x512 texture with quandrant colors."""
+"""Script that generates a texture with a checkboard pattern."""
 from argparse import ArgumentParser
+from typing import Tuple
 
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+def draw_checkerboard(
+    n_grid: int,
+    shape: Tuple[int, int, int] = (512, 512, 3),
+    color: Tuple[int, int, int] = (255, 0, 0),
+) -> np.array:
+    """Create an image that displays a texture."""
+    texture = np.zeros(shape, dtype=np.uint8)
+    # create a texture with a red background
+    texture[:, :, :] = color
+
+    # draw a checkboard pattern, filling in every other quadrant with black
+    for i in range(n_grid):
+        for j in range(n_grid):
+            if (i + j) % 2 != 0:
+                continue
+            i_size = shape[0] // n_grid
+            j_size = shape[1] // n_grid
+            texture[i * i_size : (i + 1) * i_size, j * j_size : (j + 1) * j_size, :] = 0
+
+    return texture
+
+
 def main():
     parser = ArgumentParser()
+    parser.add_argument("--n_grid", default=4, type=int, help="number of grid squares")
+    parser.add_argument("--texture_width", default=400, type=int, help="texture width")
     parser.add_argument("--output", default="texture.png", help="output file")
-    parser.add_argument("--show", action="store_true", help="show the texture")
+    parser.add_argument(
+        "--show", action="store_true", help="show the texture", default=False
+    )
     args = parser.parse_args()
 
-    texture = np.zeros((512, 512, 3), dtype=np.uint8)
-    # create a texture with a red background
-    texture[:, :, 0] = 255
+    texture = draw_checkerboard(
+        args.n_grid, (args.texture_width, args.texture_width, 3)
+    )
 
-    # draw a black cross that goes through the origin
-    width = 4
-    texture[256 - width : 256 + width, :, :] = 0
-    texture[:, 256 - width : 256 + width, :] = 0
-
-    plt.imshow(texture)
     if args.show:
+        plt.imshow(texture)
         plt.show()
     else:
         mpimg.imsave(args.output, texture)
